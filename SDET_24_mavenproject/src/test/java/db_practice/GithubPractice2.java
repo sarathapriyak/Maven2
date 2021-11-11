a9c545aabf18b0c2c352c9f58319be9565e9b22e
BASKARAN <BASKARAN@mACCC-PC>
2021-11-11 14:35:05 +0530

    My maven updated

---
+++ SDET_24_mavenproject/src/test/java/db_practice/GithubPractice2.java
@@ -0,0 +1,14 @@
+package db_practice;
+
+import org.testng.annotations.Test;
+
+public class GithubPractice2 {
+	@Test
+	
+		public void update()
+		{
+			System.out.println("edited project");
+		}
+	}
+
+
